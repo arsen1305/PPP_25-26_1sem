57fb1992fc7f52881913d3ab3f1811a014fa1148
arsen1305 <arsen.tasoev.01@bk.ru>
2025-10-31 21:16:22 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,27 @@
 
 
 if __name__ == "__main__":
-    pass # Ваш код здесь
+
+def check_brackets(s):
+    
+    stack = [] 
+    bracket_map = {')': '(', ']': '[', '}': '{'} 
+
+    for i, char in enumerate(s):
+        if char in bracket_map.values(): 
+            stack.append((char, i)) 
+        elif char in bracket_map: 
+            if not stack: 
+                return i + 1 
+            top, top_index = stack.pop()
+            if bracket_map[char] != top: 
+                return i + 1 
+    if stack: 
+        top, top_index = stack.pop()
+        return top_index + 1 
+    return "ok" 
+
+
+input_string = input("Введите строку со скобками: ")
+result = check_brackets(input_string)
+print(result) 
